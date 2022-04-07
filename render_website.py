b732e298a92f4601e4af1c0b0deacfab44a93676
RobinLoksli92 <gleb11tiun@mail.ru>
2022-04-07 12:23:43 +0200

    Add links to books txt files.

--- render_website.py
+++ render_website.py
@@ -9,16 +9,17 @@ from livereload import Server, shell
 
 
 def on_reload():
+    env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+    )
+
     template = env.get_template('template.html')
-    rendered_page = template.render(books=grouped_books,)
+    rendered_page = template.render(books=grouped_books)
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
     print('site reloaded')
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-    )
 
 books = []
 
@@ -27,7 +28,6 @@ with open('book_discription.json', 'r', encoding='utf-8') as file:
         books.append(book)
 
 grouped_books = list(chunked(books,2))
-
 
 on_reload()
 server = Server()
