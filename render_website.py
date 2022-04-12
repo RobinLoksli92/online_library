35acec0e7622648e969d33642ad66c708231c4c6
RobinLoksli92 <gleb11tiun@mail.ru>
2022-04-12 13:37:33 +0200

    Display books genres.

--- render_website.py
+++ render_website.py
@@ -2,6 +2,7 @@ from email.encoders import encode_noop
 import json
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import os
+import math
 
 from more_itertools import chunked
 from jinja2 import Environment, FileSystemLoader, select_autoescape
@@ -16,10 +17,12 @@ def on_reload():
     )
     template = env.get_template('template.html')
    
-    for i, books_group in enumerate(books_per_page, 1):
-        grouped_books = list(chunked(books_group,2))
-        rendered_page = template.render(books=grouped_books)
-        with open(f'pages/index{i}.html', 'w', encoding="utf8") as file:
+    page_count = math.ceil(len(books)/20)
+    
+    for current_page, books_group in enumerate(books_per_page, 1):
+        grouped_books = list(chunked(books_group,2))      
+        rendered_page = template.render(books=grouped_books, current_page=current_page, page_count=page_count)
+        with open(f'pages/index{current_page}.html', 'w', encoding="utf8") as file:
             file.write(rendered_page)
     print('site reloaded')
 
@@ -31,10 +34,6 @@ with open('book_discription.json', 'r', encoding='utf-8') as file:
         books.append(book)
 
 books_per_page = list(chunked(books,20))
-
-for i, book in enumerate(books_per_page, 1):
-    grouped_books = list(chunked(books_per_page[0],2))
-    print(grouped_books)
 
 on_reload()
 server = Server()
