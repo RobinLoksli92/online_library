07ee82ead968dba1c941e9894a276e5f88c32d92
RobinLoksli92 <gleb11tiun@mail.ru>
2022-04-14 15:08:46 +0200

    add function to get image name

--- render_website.py
+++ render_website.py
@@ -1,13 +1,19 @@
-from email.encoders import encode_noop
 import json
-from http.server import HTTPServer, SimpleHTTPRequestHandler
 import os
 import math
+from urllib.parse import urlparse
 
 from more_itertools import chunked
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-from livereload import Server, shell
+from livereload import Server
+
+
+def get_img_name(books):
+    for book in books:
+        img_path = urlparse(book['Картинка']).path
+        img_name = os.path.basename(img_path)
+        book['img_name'] = img_name
 
 
 def on_reload():
@@ -17,8 +23,16 @@ def on_reload():
     )
     template = env.get_template('template.html')
    
+    books = []
+
+    with open('media/book_discription.json', 'r', encoding='utf-8') as file:
+        for book in json.load(file):
+            books.append(book)
+
+    books_per_page = list(chunked(books,20))
+    get_img_name(books)
     page_count = math.ceil(len(books)/20)
-    
+
     for current_page, books_group in enumerate(books_per_page, 1):
         grouped_books = list(chunked(books_group,2))      
         rendered_page = template.render(books=grouped_books, current_page=current_page, page_count=page_count)
@@ -27,15 +41,13 @@ def on_reload():
     print('site reloaded')
 
 
-books = []
+def main():
+    server = Server()
+    on_reload()
+    server.watch('template.html', on_reload)
+    server.serve(root='.')
+    
 
-with open('book_discription.json', 'r', encoding='utf-8') as file:
-    for book in json.load(file):
-        books.append(book)
 
-books_per_page = list(chunked(books,20))
-
-on_reload()
-server = Server()
-server.watch('template.html', on_reload)
-server.serve(root='.')
+if __name__ == '__main__':
+    main()
