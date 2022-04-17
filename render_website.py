bb1eab608a45553c825c2464f32178e01eadf9dc
RobinLoksli92 <gleb11tiun@mail.ru>
2022-04-17 22:48:32 +0200

    Upgrades

--- render_website.py
+++ render_website.py
@@ -1,12 +1,16 @@
 import json
-import os
 import math
+import os
 from urllib.parse import urlparse
 
-from more_itertools import chunked
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-
 from livereload import Server
+from more_itertools import chunked
+
+
+BOOKS_PER_PAGE = 20
+
+COL_NUMBER = 2
 
 
 def get_img_name(books):
@@ -18,26 +22,31 @@ def get_img_name(books):
 
 def on_reload():
     env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
     )
     template = env.get_template('template.html')
-   
-    books = []
 
     with open('media/book_discription.json', 'r', encoding='utf-8') as file:
-        for book in json.load(file):
-            books.append(book)
+        books = json.load(file)
 
-    books_per_page = list(chunked(books,20))
+    books_per_page = list(chunked(books, BOOKS_PER_PAGE))
     get_img_name(books)
-    page_count = math.ceil(len(books)/20)
+    page_count = math.ceil(len(books)/BOOKS_PER_PAGE)
 
     for current_page, books_group in enumerate(books_per_page, 1):
-        grouped_books = list(chunked(books_group,2))      
-        rendered_page = template.render(books=grouped_books, current_page=current_page, page_count=page_count)
-        with open(f'pages/index{current_page}.html', 'w', encoding="utf8") as file:
+        grouped_books = list(chunked(books_group, COL_NUMBER))
+        rendered_page = template.render(
+            books=grouped_books,
+            current_page=current_page,
+            page_count=page_count)
+        with open(
+                f'pages/index{current_page}.html',
+                'w',
+                encoding="utf8"
+                ) as file:
             file.write(rendered_page)
+
     print('site reloaded')
 
 
@@ -46,7 +55,7 @@ def main():
     on_reload()
     server.watch('template.html', on_reload)
     server.serve(root='.')
-    
+
 
 if __name__ == '__main__':
     main()
